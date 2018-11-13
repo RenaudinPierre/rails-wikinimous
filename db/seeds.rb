d0810c052042b47b8e9cb59dcd4f6bbc9ce02377
Pierre Renaudin <renaudin.pierre@gmail.com>
2018-11-13 17:36:34 +0100

    exercice3

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do
+  Article.create(
+    title: Faker::Job.title,
+    content: Faker::Bitcoin.address
+  )
+end
+
+
+
+
